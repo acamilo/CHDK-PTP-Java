3214771fd8b224409fa2a96b906cf6283deefd97
Mikolaj Dobski <mikolajd@man.poznan.pl>
2014-04-03 16:11:08 +0200

    forced the USBImageHello to switch the camera into capture mode before acquiring display data reworked sysouts to debug output

--- src/main/java/usb/USBImageHello.java
+++ src/main/java/usb/USBImageHello.java
@@ -13,12 +13,18 @@ import javax.usb.UsbHub;
 import javax.usb.UsbInterface;
 import javax.usb.UsbServices;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import packet.CHDKScreenImage;
 import packet.PTPPacket;
 import camera.PTPSession;
 import camera.displayImage;
 
 public class USBImageHello {
+
+    private static Log log = LogFactory.getLog(USBImageHello.class);
+
     public static UsbDevice findDevice(UsbHub hub, short vendorId, short productId) {
         for (UsbDevice device : (List<UsbDevice>) hub.getAttachedUsbDevices()) {
             UsbDeviceDescriptor desc = device.getUsbDeviceDescriptor();
@@ -41,19 +47,19 @@ public class USBImageHello {
         short canonSX50 = 0x3259;
         UsbDevice dev = findDevice(rootHub, canonVendor, canonSX50);
         if (dev == null) {
-            System.out.println("No Device Found!");
+            log.debug("No Device Found!");
             return;
         }
-        System.out.println("Found Matching Device!:\t" + dev);
+        log.debug("Found Matching Device!:\t" + dev);
 
         try {
             // device info
-            System.out.println("\tManufacturer:\t" + dev.getManufacturerString());
-            System.out.println("\tSerial Number:\t" + dev.getSerialNumberString());
+            log.debug("\tManufacturer:\t" + dev.getManufacturerString());
+            log.debug("\tSerial Number:\t" + dev.getSerialNumberString());
 
             // Get dev config
             UsbConfiguration config = dev.getActiveUsbConfiguration();
-            System.out.println("\tGot Device Configuration:\t" + config);
+            log.debug("\tGot Device Configuration:\t" + config);
 
             // Get interfaces
             List totalInterfaces = config.getUsbInterfaces();
@@ -61,22 +67,22 @@ public class USBImageHello {
             UsbEndpoint camOut = null;
             for (int i = 0; i < totalInterfaces.size(); i++) {
                 UsbInterface interf = (UsbInterface) totalInterfaces.get(i);
-                System.out.println("\t\tFound Interface:\t" + interf);
+                log.debug("\t\tFound Interface:\t" + interf);
                 interf.claim();
                 List totalEndpoints = interf.getUsbEndpoints();
 
                 for (int j = 0; j < totalEndpoints.size(); j++) {
                     UsbEndpoint ep = (UsbEndpoint) totalEndpoints.get(j);
-                    // System.out.println("\t\t\tFound Endpoint:\n\t\t\t\t"+ep.getUsbEndpointDescriptor().toString().replaceAll("[\\n\\r]+",
-                    // "\n\t\t\t\t"));
+                    log.debug("\t\t\tFound Endpoint:\n\t\t\t\t"
+                            + ep.getUsbEndpointDescriptor().toString()
+                                    .replaceAll("[\\n\\r]+", "\n\t\t\t\t"));
                     // We're looking for a bulk In and bulk out
                     if (ep.getDirection() == -128 && ep.getType() == 2) {
-                        System.out.println("\t\t\tAssigning Bulk In endpoint #" + j + " to camIn");
+                        log.debug("\t\t\tAssigning Bulk In endpoint #" + j + " to camIn");
                         camIn = ep; // Bulk IN endpoint
                     }
                     if (ep.getDirection() == 0 && ep.getType() == 2) {
-                        System.out
-                                .println("\t\t\tAssigning Bulk OUT endpoint #" + j + " to camOut");
+                        log.debug("\t\t\tAssigning Bulk OUT endpoint #" + j + " to camOut");
                         camOut = ep; // Bulk Out endpoint
                     }
 
@@ -84,28 +90,46 @@ public class USBImageHello {
 
             }
             if (camIn == null || camOut == null)
-                System.out.println("Didn't find my endpoints Something verry bad happened..");
+                log.debug("Didn't find my endpoints Something verry bad happened..");
             else
-                System.out.println("\tFound my endpoints, Building pipe");
+                log.debug("\tFound my endpoints, Building pipe");
 
+            log.debug("Starting PTP session");
             PTPSession session = new PTPSession(camIn, camOut);
 
             PTPPacket p = new PTPPacket(PTPPacket.PTP_USB_CONTAINER_COMMAND,
                     PTPPacket.PTP_OPPCODE_OpenSession, 0, new byte[] { 0x01, 0x00, 0x00, 0x00 });
+            log.debug("Sending CHDK start package: " + p);
             session.sendPTPPacket(p);
-            session.getResponse();
+            log.debug("response: " + session.getResponse());
+
+            p = new PTPPacket(PTPPacket.PTP_USB_CONTAINER_COMMAND, PTPPacket.PTP_OPPCODE_CHDK, 1,
+                    new byte[] { 7, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 });
+            log.debug("Sending script mode start package: " + p);
+            session.sendPTPPacket(p); // IMPORTANT: NO RESPONSE!!!
+
+            p = new PTPPacket(PTPPacket.PTP_USB_CONTAINER_DATA, PTPPacket.PTP_OPPCODE_CHDK, 2,
+                    "switch_mode_usb(1)\0".getBytes());
+            log.debug("switching to capture mode: " + p);
+            session.sendPTPPacket(p); // IMPORTANT: NO RESPONSE!!!
+
             displayImage d = null;
             while (true) {
                 p = new PTPPacket(PTPPacket.PTP_USB_CONTAINER_COMMAND, PTPPacket.PTP_OPPCODE_CHDK,
                         1, new byte[] { 12, 0x00, 0x00, 0x00, (byte) (0x01), 0x00, 0x00, 0x00 });
+                log.debug("Sending getDisplayData command: " + p);
                 session.sendPTPPacket(p);
+
                 /*
-                 * p = new PTPPacket(PTPPacket.PTP_USB_CONTAINER_DATA,
-                 * PTPPacket.PTP_OPPCODE_CHDK, 2, "switch_mode_usb(1)\0".getBytes());
-                 * session.sendPTPPacket(p);
+                 * // capture mode done before loop p = new
+                 * PTPPacket(PTPPacket.PTP_USB_CONTAINER_DATA, PTPPacket.PTP_OPPCODE_CHDK,
+                 * 2, "switch_mode_usb(1)\0".getBytes());
+                 * log.debug("switching to capture mode: " + p); session.sendPTPPacket(p);
+                 * // IMPORTANT: NO RESPONSE!!!
                  */
 
                 p = session.getResponse();
+                log.debug("Response for display data request: " + p);
                 CHDKScreenImage i = new CHDKScreenImage(p.getData());
                 if (d == null)
                     d = new displayImage(i);
@@ -124,5 +148,4 @@ public class USBImageHello {
         }
 
     }
-
 }
